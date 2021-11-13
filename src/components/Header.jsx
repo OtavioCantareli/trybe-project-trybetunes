ca1419beffad4a08ad64b829217fda5a87ab8c74
Otavio Cantareli <otavio_cantareli@outlook.com>
2021-11-13 10:26:28 -0300

    Requisito 4 - parcial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 export default class Header extends React.Component {
@@ -9,7 +10,16 @@ export default class Header extends React.Component {
   render() {
     return (
       <header data-testid="header-component">
-        <span data-testid="header-user-name">{() => getUser()}</span>
+        <Link to="/search" data-testid="link-to-search">
+          Pesquisar
+        </Link>
+        <Link to="/favorites" data-testid="link-to-favorites">
+          Favoritos
+        </Link>
+        <Link to="/profile" data-testid="link-to-profile">
+          Perfil
+        </Link>
+        {/* <span data-testid="header-user-name">{() => getUser()}</span> */}
       </header>
     );
   }
