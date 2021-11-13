a9ced305b39e83e848befc08cf92e73fb3095a3d
Otavio Cantareli <otavio_cantareli@outlook.com>
2021-11-12 21:02:21 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  constructor() {
+    super();
+    this.onChange = this.onChange.bind(this);
+    this.state = {
+      value: '',
+    };
+  }
+
+  onChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
+    const { value } = this.state;
+    const { onChange } = this;
+    const length = 2;
     return (
-      <div data-testid="page-search" />
+      <>
+        <Header />
+        <div data-testid="page-search">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Name of band?"
+            onChange={ onChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            value={ value }
+            disabled={ value.length < length }
+          >
+            Pesquisar
+          </button>
+        </div>
+      </>
     );
   }
 }
